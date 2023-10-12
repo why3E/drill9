b81acd4ce747c96408b215184614a4329fbf7b26
wonju <jwwon00@naver.com>
2023-10-12 23:15:24 +0900

    drill9 제출 2019182025 원정우

--- boy.py
+++ boy.py
@@ -167,7 +167,7 @@ class StateMachine:
         self.cur_state = Idle
 
         self.table = {
-            Idle: {right_down: Run, left_down: Run, left_up: Idle, right_up: Idle, time_out: Sleep,
+            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep,
                    a_key_down: AutoRun},
             Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
             Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle},
