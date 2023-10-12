dd7ceffa034d042461adbd491d8072e1ed27d747
wonju <jwwon00@naver.com>
2023-10-12 20:11:51 +0900

    달리는 도중 AutoRun 삭제

--- boy.py
+++ boy.py
@@ -167,9 +167,9 @@ class StateMachine:
 
         self.table = {
             Idle: {right_down: Run, left_down: Run, left_up: Idle, right_up: Idle, time_out: Sleep, a_key_down: AutoRun},
-            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, a_key_down: AutoRun},
+            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
             Sleep: {right_down: Run, left_down: Run, right_up: Idle, left_up: Idle, space_down: Idle},
-            AutoRun: {right_down: Run, left_down: Run, right_up: Idle, left_up: Idle, space_down: Idle, time_out: Idle}
+            AutoRun: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, time_out: Idle}
         }
 
     def start(self):
